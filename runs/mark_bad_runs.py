dd67f5a728803725ce1767a1acf1a531b05aafc2
Eli Chertkov <chertkv2@illinois.edu>
2020-02-23 12:23:47 -0600

    Fixed typos in mark bad runs script.

--- runs/mark_bad_runs.py
+++ runs/mark_bad_runs.py
@@ -1,11 +1,12 @@
 import os
 
-num_runs = 1600
+num_badfiles = 0
+num_runs     = 480
 for ind_run in range(num_runs):
-    folder_name  = 'run2D_constant_2_{}'.format(ind_run)
+    folder_name  = 'run3D_constant_3_{}'.format(ind_run)
     out_filename = '{}/out.txt'.format(folder_name)
 
-    out_file = open(out_filename, 'w')
+    out_file = open(out_filename, 'r')
 
     indW          = -1
     ind_expansion = -1
@@ -16,9 +17,10 @@ for ind_run in range(num_runs):
                 result_filename     = '{}/{}.p'.format(folder_name, indW)
                 new_result_filename = '{}/{}_bad.p'.format(folder_name, indW)
 
-                if os.isfile(result_filename):
-                    #os.rename(result_filename, new_result_filename)
+                if os.path.isfile(result_filename):
+                    os.rename(result_filename, new_result_filename)
                     print('{} is bad! Renamed it to {}'.format(result_filename, new_result_filename), flush=True)
+                    num_badfiles += 1
             indW         += 1
             ind_expansion = -1
             currW_good    = True
@@ -29,5 +31,18 @@ for ind_run in range(num_runs):
         # the memory was emptied.
         if "vmem after emptying" in line:
             currW_good = False
-            
+    
     out_file.close()
+    
+    # An extra check for the final file in the list.
+    if not currW_good:
+        result_filename     = '{}/{}.p'.format(folder_name, indW)
+        new_result_filename = '{}/{}_bad.p'.format(folder_name, indW)
+        
+        if os.path.isfile(result_filename):
+            os.rename(result_filename, new_result_filename)
+            print('{} is bad! Renamed it to {}'.format(result_filename, new_result_filename), flush=True)
+            num_badfiles += 1
+
+print('Number of bad files found: {}'.format(num_badfiles), flush=True)
+
